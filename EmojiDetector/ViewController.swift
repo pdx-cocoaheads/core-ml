7fb81538a2416e7afe441fca4517d9737a72e87f
Ryan Arana <ryan.arana@gmail.com>
2017-08-09 15:05:13 -0700

    Add TextView and get a skeleton app ready to do some Text analysis.

--- EmojiDetector/ViewController.swift
+++ EmojiDetector/ViewController.swift
@@ -11,73 +11,21 @@
 
 
 import UIKit
-import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var resultsLabel: UILabel!
-    @IBOutlet weak var cameraView: UIView!
-    private var captureSession: AVCaptureSession!
-    private var videoPreviewLayer: AVCaptureVideoPreviewLayer!
     private let processingQueue: DispatchQueue = DispatchQueue(label: "videoProcessingQueue")
-
-    private var lastTimestamp = CMTime()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else {
-            assertionFailure("Failed to get default capture device.")
-            return
-        }
-
-        captureSession = AVCaptureSession()
-
-        do {
-            let input = try AVCaptureDeviceInput(device: captureDevice)
-            captureSession.addInput(input)
-
-            let output = AVCaptureVideoDataOutput()
-            output.setSampleBufferDelegate(self, queue: processingQueue)
-            output.videoSettings = [
-                kCVPixelBufferPixelFormatTypeKey as String: NSNumber(value: kCVPixelFormatType_32BGRA)
-            ]
-            output.alwaysDiscardsLateVideoFrames = true
-            captureSession.addOutput(output)
-
-            // We want the buffers to be in portrait orientation otherwise they are
-            // rotated by 90 degrees. Need to set this _after_ addOutput()!
-            output.connection(with: .video)?.videoOrientation = .portrait
-
-            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-            videoPreviewLayer.videoGravity = .resizeAspectFill
-            videoPreviewLayer.frame = view.layer.bounds
-            cameraView.layer.addSublayer(videoPreviewLayer)
-
-            captureSession.startRunning()
-        } catch {
-            print(error)
-        }
+        textView.text = nil
+        textView.delegate = self
     }
 }
 
-extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
-    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        // Because lowering the capture device's FPS looks ugly in the preview,
-        // we capture at full speed but only call the delegate at its desired
-        // framerate.
-        let timestamp = CMSampleBufferGetPresentationTimeStamp(sampleBuffer)
-        let deltaTime = timestamp - lastTimestamp
-        if deltaTime >= CMTimeMake(1, Int32(15)) {
-            lastTimestamp = timestamp
-            let imageBuffer = CMSampleBufferGetImageBuffer(sampleBuffer)
-
-            // TODO: Analyze dat imageBuffer.
-            let result = "⁉️"
-            DispatchQueue.main.async {
-                // Update UI here.
-                self.resultsLabel.text = result
-            }
-        }
+extension ViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        
     }
 }
-
