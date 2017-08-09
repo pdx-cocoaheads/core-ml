f00a79d4a42a6afd00c7a496fc18b1eaee3fb761
Ryan Arana <ryan.arana@gmail.com>
2017-08-09 16:24:11 -0700

    Add functionality to make the text analysis work.

--- EmojiDetector/ViewController.swift
+++ EmojiDetector/ViewController.swift
@@ -11,21 +11,92 @@
 
 
 import UIKit
+import CoreML
 
 class ViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var resultsLabel: UILabel!
+    @IBOutlet weak var labelBottomConstraint: NSLayoutConstraint!
     private let processingQueue: DispatchQueue = DispatchQueue(label: "videoProcessingQueue")
+
+    private let model = SentimentPolarity()
+    private let options: NSLinguisticTagger.Options = [.omitWhitespace, .omitPunctuation, .omitOther]
+    private lazy var tagger = NSLinguisticTagger(
+        tagSchemes: NSLinguisticTagger.availableTagSchemes(forLanguage: "en"),
+        options: Int(self.options.rawValue)
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
         textView.text = nil
         textView.delegate = self
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardDidShow(notification:)),
+            name: .UIKeyboardDidShow,
+            object: nil
+        )
+
+        textView.becomeFirstResponder()
+        resultsLabel.text = "👋"
+    }
+
+    @objc private func keyboardDidShow(notification: Notification) {
+        guard let keyboardRect = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as AnyObject).cgRectValue else { return }
+        labelBottomConstraint.constant = -keyboardRect.size.height - 8
+        view.layoutIfNeeded()
     }
 }
 
 extension ViewController: UITextViewDelegate {
     func textViewDidChange(_ textView: UITextView) {
-        
+        guard let text = textView.text else {
+            resultsLabel.text = "😑"
+            return
+        }
+
+        guard text.count > 3 else {
+            resultsLabel.text = "🤔"
+            return
+        }
+
+        var input = [String: Double]()
+        tagger.string = text
+        let range = NSRange(location: 0, length: text.utf16.count)
+
+        tagger.enumerateTags(in: range, scheme: .nameType, options: options) { _, tokenRange, _, _ in
+            let token = (text as NSString).substring(with: tokenRange).lowercased()
+            guard token.count >= 3 else { return }
+
+            input[token] = input[token, default: 0] + 1
+        }
+
+        let output = try! model.prediction(input: input)
+        switch output.classLabel {
+        case "Pos":
+            resultsLabel.text = positiveEmoji(for: output.classProbability[output.classLabel, default: 0])
+        case "Neg":
+            resultsLabel.text = negativeEmoji(for: output.classProbability[output.classLabel, default: 0])
+        default:
+            resultsLabel.text = output.classLabel
+        }
+    }
+
+    private func positiveEmoji(for confidence: Double) -> String {
+        switch confidence {
+        case 0.25..<0.45: return "🙂"
+        case 0.45..<0.55: return "😁"
+        case 0.55...: return "😂"
+        default: return "🤔"
+        }
+    }
+
+    private func negativeEmoji(for confidence: Double) -> String {
+        switch confidence {
+        case 0.25..<0.45: return "😕"
+        case 0.45..<0.55: return "☹️"
+        case 0.55...: return "😡"
+        default: return "🤔"
+        }
     }
 }
